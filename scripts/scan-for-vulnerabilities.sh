c688db6e272919a2ec2617cd6f6e0e6f49aad7cc
Januda Bethmin <desilvabethmin@gmail.com>
2024-08-09 13:06:55 +0530

    renamed files

--- scripts/scan-for-vulnerabilities.sh
+++ scripts/scan-for-vulnerabilities.sh
@@ -1,11 +1,11 @@
 # Creates a file to capture standard out
 # Removes the file if it exists
-rm -rf npm_install_output.txt
+rm -rf npm-install-output.txt
 # Creates a blank file
-touch npm_install_output.txt
+touch npm-install-output.txt
 
 # Run NPM install (Mentions vulnerabilities at the end). Output goes into file.
-npm install >> npm_install_output.txt
+npm install >> npm-install-output.txt
 
 # Catch all vulnerabilities
 # search_term="found [0-9]{1,6} vulnerabilities ([0-9]{1,6} low, [0-9]{1,6} moderate, [0-9]{1,6} high)"
@@ -13,9 +13,9 @@ npm install >> npm_install_output.txt
 # Only high vulnerabilities
 search_term=", [0-9]{1,6} high)"
 
-if grep -REo "$only_high_vulnerabilities" npm_install_output.txt
+if grep -REo "$only_high_vulnerabilities" npm-install-output.txt
 then
-  if ! grep -o "found 0 vulnerabilities" npm_install_output.txt
+  if ! grep -o "found 0 vulnerabilities" npm-install-output.txt
   then
     echo "Yes, security vulnerabilities found."
     exit 1
